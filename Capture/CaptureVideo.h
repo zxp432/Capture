ad9ab5b2144c2c4327bb58faca3a302b91525895
saiohao <442406628@qq.com>
2016-03-26 18:03:50 +0800

    2016.3.26——周昊

--- Capture/CaptureVideo.h
+++ Capture/CaptureVideo.h
@@ -64,12 +64,12 @@ namespace Capture1 {
 
 	#pragma region Window_control
 	private: System::Windows::Forms::Panel^  panel1;
-	private: System::Windows::Forms::GroupBox^  groupBox4;
-	private: System::Windows::Forms::Label^  label11;
-	private: System::Windows::Forms::Label^  label9;
-	private: System::Windows::Forms::Label^  label8;
-	private: System::Windows::Forms::Label^  label7;
-	private: System::Windows::Forms::Label^  label6;
+
+
+
+
+
+
 	private: System::Windows::Forms::GroupBox^  groupBox3;
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::ComboBox^  comboBox1;
@@ -94,12 +94,6 @@ namespace Capture1 {
 		{
 			this->components = (gcnew System::ComponentModel::Container());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
-			this->label11 = (gcnew System::Windows::Forms::Label());
-			this->label9 = (gcnew System::Windows::Forms::Label());
-			this->label8 = (gcnew System::Windows::Forms::Label());
-			this->label7 = (gcnew System::Windows::Forms::Label());
-			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
@@ -110,7 +104,6 @@ namespace Capture1 {
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->timer2 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->panel1->SuspendLayout();
-			this->groupBox4->SuspendLayout();
 			this->groupBox3->SuspendLayout();
 			this->groupBox1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
@@ -120,90 +113,13 @@ namespace Capture1 {
 			// panel1
 			// 
 			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->panel1->Controls->Add(this->groupBox4);
-			this->panel1->Controls->Add(this->groupBox3);
 			this->panel1->Controls->Add(this->groupBox1);
+			this->panel1->Controls->Add(this->groupBox3);
 			this->panel1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->panel1->Location = System::Drawing::Point(3, 3);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(353, 513);
+			this->panel1->Size = System::Drawing::Size(536, 626);
 			this->panel1->TabIndex = 0;
-			// 
-			// groupBox4
-			// 
-			this->groupBox4->Controls->Add(this->label11);
-			this->groupBox4->Controls->Add(this->label9);
-			this->groupBox4->Controls->Add(this->label8);
-			this->groupBox4->Controls->Add(this->label7);
-			this->groupBox4->Controls->Add(this->label6);
-			this->groupBox4->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->groupBox4->Location = System::Drawing::Point(3, 368);
-			this->groupBox4->Name = L"groupBox4";
-			this->groupBox4->Size = System::Drawing::Size(345, 140);
-			this->groupBox4->TabIndex = 11;
-			this->groupBox4->TabStop = false;
-			this->groupBox4->Text = L"Video Properties:";
-			// 
-			// label11
-			// 
-			this->label11->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->label11->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label11->Location = System::Drawing::Point(5, 109);
-			this->label11->Name = L"label11";
-			this->label11->Size = System::Drawing::Size(335, 25);
-			this->label11->TabIndex = 6;
-			this->label11->Text = L"Video Frame Rate:";
-			this->label11->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			// 
-			// label9
-			// 
-			this->label9->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->label9->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label9->Location = System::Drawing::Point(5, 81);
-			this->label9->Name = L"label9";
-			this->label9->Size = System::Drawing::Size(335, 25);
-			this->label9->TabIndex = 5;
-			this->label9->Text = L"Video Resolution:";
-			this->label9->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			// 
-			// label8
-			// 
-			this->label8->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->label8->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label8->Location = System::Drawing::Point(5, 53);
-			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(334, 25);
-			this->label8->TabIndex = 4;
-			this->label8->Text = L"Frame No.:";
-			this->label8->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			// 
-			// label7
-			// 
-			this->label7->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->label7->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label7->Location = System::Drawing::Point(175, 25);
-			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(165, 25);
-			this->label7->TabIndex = 3;
-			this->label7->Text = L"Time:";
-			this->label7->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			// 
-			// label6
-			// 
-			this->label6->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->label6->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label6->Location = System::Drawing::Point(5, 25);
-			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(165, 25);
-			this->label6->TabIndex = 2;
-			this->label6->Text = L"Codec:";
-			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
 			// 
 			// groupBox3
 			// 
@@ -213,18 +129,18 @@ namespace Capture1 {
 				static_cast<System::Byte>(0)));
 			this->groupBox3->Location = System::Drawing::Point(3, 3);
 			this->groupBox3->Name = L"groupBox3";
-			this->groupBox3->Size = System::Drawing::Size(344, 62);
+			this->groupBox3->Size = System::Drawing::Size(524, 62);
 			this->groupBox3->TabIndex = 10;
 			this->groupBox3->TabStop = false;
-			this->groupBox3->Text = L"Source";
+			this->groupBox3->Text = L"检测源";
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(249, 22);
+			this->button2->Location = System::Drawing::Point(436, 23);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(79, 30);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"Start";
+			this->button2->Text = L"开始";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
@@ -232,10 +148,10 @@ namespace Capture1 {
 			// 
 			this->comboBox1->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Capture From Camera", L"Capture From File" });
-			this->comboBox1->Location = System::Drawing::Point(8, 23);
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"打开摄像头进行检测", L"选择视频进行检测" });
+			this->comboBox1->Location = System::Drawing::Point(13, 23);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(235, 27);
+			this->comboBox1->Size = System::Drawing::Size(417, 27);
 			this->comboBox1->TabIndex = 0;
 			// 
 			// groupBox1
@@ -244,30 +160,31 @@ namespace Capture1 {
 			this->groupBox1->Controls->Add(this->pictureBox1);
 			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->groupBox1->Location = System::Drawing::Point(3, 65);
+			this->groupBox1->Location = System::Drawing::Point(5, 61);
 			this->groupBox1->Margin = System::Windows::Forms::Padding(5);
 			this->groupBox1->Name = L"groupBox1";
 			this->groupBox1->Padding = System::Windows::Forms::Padding(5);
-			this->groupBox1->Size = System::Drawing::Size(345, 303);
+			this->groupBox1->Size = System::Drawing::Size(522, 558);
 			this->groupBox1->TabIndex = 9;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Original Video:";
+			this->groupBox1->Text = L"视频";
 			// 
 			// trackBar1
 			// 
 			this->trackBar1->AutoSize = false;
-			this->trackBar1->Location = System::Drawing::Point(8, 270);
+			this->trackBar1->Location = System::Drawing::Point(13, 534);
 			this->trackBar1->Name = L"trackBar1";
-			this->trackBar1->Size = System::Drawing::Size(322, 26);
+			this->trackBar1->Size = System::Drawing::Size(501, 26);
 			this->trackBar1->TabIndex = 1;
+			this->trackBar1->Visible = false;
 			this->trackBar1->Scroll += gcnew System::EventHandler(this, &Form1::trackBar1_Scroll);
 			// 
 			// pictureBox1
 			// 
 			this->pictureBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->pictureBox1->Location = System::Drawing::Point(10, 25);
+			this->pictureBox1->Location = System::Drawing::Point(12, 38);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(320, 240);
+			this->pictureBox1->Size = System::Drawing::Size(500, 500);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
@@ -290,7 +207,7 @@ namespace Capture1 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(10, 23);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(359, 519);
+			this->ClientSize = System::Drawing::Size(542, 632);
 			this->Controls->Add(this->panel1);
 			this->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
@@ -301,9 +218,8 @@ namespace Capture1 {
 			this->Name = L"Form1";
 			this->Padding = System::Windows::Forms::Padding(3);
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"Capture";
+			this->Text = L"实时物体检测";
 			this->panel1->ResumeLayout(false);
-			this->groupBox4->ResumeLayout(false);
 			this->groupBox3->ResumeLayout(false);
 			this->groupBox1->ResumeLayout(false);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->EndInit();
@@ -319,24 +235,28 @@ namespace Capture1 {
 		{
 			MessageBox::Show(this,"Select Capture Method","Error!!!");
 		}
-		if(button2->Text == "Start")
+		if(button2->Text == "开始")
 		{
-			if (comboBox1->Text == "Capture From Camera")
+			if (comboBox1->Text == "打开摄像头进行检测")
 			{					
 				capture = cvCaptureFromCAM(0);
 				fps = 30;// cvGetCaptureProperty(capture, CV_CAP_PROP_FPS); //视频帧率
 				fame_continue = ceil(fps / 5);
+				trackBar1->Visible = false;
 				trackBar1->Minimum = 0;
 				trackBar1->Maximum = 0;
-				button2->Text = "Stop";
+				button2->Text = "停止";
 				timer1->Start();
+				pictureBox1->Location = System::Drawing::Point(12, 38);
 			}
-			else if (comboBox1->Text == "Capture From File")
+			else if (comboBox1->Text == "选择视频进行检测")
 			{
 				openFileDialog1->Filter = "AVI files (*.avi)|*.txt|All files (*.*)|*.*";
 				openFileDialog1->FilterIndex = 2;
 				openFileDialog1->RestoreDirectory = true;
 				openFileDialog1->FileName ="";
+				trackBar1->Visible = true;
+				pictureBox1->Location = System::Drawing::Point(12, 28);
 				if ( openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK )
 				{		
 					char *fileName = (char*) Marshal::StringToHGlobalAnsi(openFileDialog1->FileName).ToPointer();					
@@ -345,15 +265,15 @@ namespace Capture1 {
 					fame_continue = ceil(fps / 5);
 					trackBar1->Minimum = 0;
 					trackBar1->Maximum = (int)cvGetCaptureProperty(capture,CV_CAP_PROP_FRAME_COUNT);
-					button2->Text = "Stop";
+					button2->Text = "停止";
 					timer1->Start();
 				}				
 			}
 		}
-		else if(button2->Text == "Stop")
+		else if(button2->Text == "停止")
 		{
 			cvReleaseCapture(&capture);		
-			button2->Text = "Start";
+			button2->Text = "开始";
 			timer1->Stop();
 			timer2->Stop();
 			now_frame_no = 0;
@@ -424,10 +344,6 @@ namespace Capture1 {
 //					trackBar1->Value = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_POS_FRAMES);
 					double codec_double = cvGetCaptureProperty(capture, CV_CAP_PROP_FOURCC);
 					//label6->Text = "Codec: " + System::Text::Encoding::UTF8->GetString(BitConverter::GetBytes((int)codec_double));
-					label7->Text = "Time: " + (TimeSpan::FromMilliseconds(cvGetCaptureProperty(capture, CV_CAP_PROP_POS_MSEC)).ToString())->Substring(0, 8);
-					label8->Text = "Frame No.: " + (int)cvGetCaptureProperty(capture, CV_CAP_PROP_POS_FRAMES);
-					label9->Text = "Video Resolution: " + (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT) + " X " + (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-					label11->Text = "Video Frame Rate: " + (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
 					now_frame_no++;
 				}
 			}
@@ -456,7 +372,6 @@ namespace Capture1 {
 				temp->cls = box[5]+":"+box[4];
 				boxes[i] = temp;
 			}
-			label6->Text = "no clear";
 		}
 		else
 		{
@@ -469,7 +384,6 @@ namespace Capture1 {
 			temp->cls = "";
 			temp->score = "";
 			boxes[0] = temp;
-			label6->Text = "clear "+ boxes->Length;
 		}
 		client->closeSocket();
 		ReleaseMutex(hMutex);
